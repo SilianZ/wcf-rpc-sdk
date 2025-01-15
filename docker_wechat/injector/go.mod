e884279bdde88d0e3767368508541bf139d5038e
Clov614 <clov614@gmail.com>
2025-01-15 17:18:00 +0800

    chore: 移除无用的文件和配置

--- docker_wechat/injector/go.mod
+++
@@ -1,3 +0,0 @@
-module injector
-
-go 1.21.12
--- docker_wechat/res/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# 启动微信 (如果 entrypoint.sh 中没有启动微信)
-echo "Starting WeChat..."
-wine "/root/.wine/drive_c/Program Files/Tencent/WeChat/WeChat.exe"
-
